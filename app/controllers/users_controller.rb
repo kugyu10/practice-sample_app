7a7bd255cf3ea778f8f329fad4b1dcc240dd6f20
kugyu10 <kugyu10@gmail.com>
2018-04-30 04:07:30 +0000

    ユーザー登録機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,44 @@
 class UsersController < ApplicationController
-  def new
+
+  def index
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new 
+  end
+  
+  
+  # POST /users
+  def create
+    
+    #@user = User.new(params[:user])
+    @user = User.new(user_params)
+    
+    if @user.save
+      # 成功
+      flash[:success] = "Wellcome to Sample App"
+      redirect_to @user # redirect_to user_path(@user.id)の省略形
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
+
+  resources :users
+
   
   get '/home',    to: 'static_pages#home'
   get '/help',    to: 'static_pages#help'
