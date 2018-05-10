e8af2c52793f1a27c897b44a7900c32fda18a563
kugyu10 <kugyu10@gmail.com>
2018-05-10 07:15:25 +0000

    パスワード再設定メール追加

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,8 +7,6 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    @greeting = "Hi"
-
     mail to: user.email, subject: "アカウント有効化_SampleApp"
   end
 
@@ -17,9 +15,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "パスワード再設定_SampleApp"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   get '/login',     to:'sessions#new'
   post '/login',    to:'sessions#create'
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   resources :users
   resources :account_activations, only: [:edit]
-  
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
