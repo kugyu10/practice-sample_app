a6d88efc17e060322286f2cfa015b0cd6304948b
kugyu10 <kugyu10@gmail.com>
2018-05-21 22:53:35 +0000

    followボタンバグfix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,14 @@ class RelationshipsController < ApplicationController
 
   #current_userがuserをフォローする
   def create
-    user = User.find(params[:id])
+    user = User.find(params[:followed_id])
     current_user.follow(user)
     redirect_to user
   end
   
   #current_userがuserをフォロー解除する
   def destroy
-    user = User.find(params[:id])
+    user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
     redirect_to user
   end
